b5a5254975841e9ab16857f46be455758d6e09f0
AnujUsrethe123 <anujusrethe69@gmail.com>
2025-01-14 06:18:34 -0800

    code

---
+++ day 16/index.js
@@ -0,0 +1,50 @@
+// console.log('hehheee');
+
+// primary;-
+//  number
+// a=5.5
+// String 'c' 'cc' "ccc"
+// Boolean true ,false  0,1
+// null
+// undefined
+// Symbol
+// BigInt
+// Int a
+
+// var, let ,const
+
+// var a=5
+// console.log(a);
+
+// var a=5
+// var a=10 
+// console.log(a)
+
+// let a=5
+// a=11
+// console.log(a);
+
+
+// const a=10
+// console.log(a);
+
+// console.log(0.1+0.2==0.3);
+
+// var a=10
+// var to =a==10
+// console.log(to);
+
+
+// let a='rahul'
+// let b='jain'
+// console.log(`${a} ${b}`);
+
+
+// = == ===
+
+
+// console.log();
+
+// console.log(false=='false');
+
+
